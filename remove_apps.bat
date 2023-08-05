14498fd8693a05b36679acdfb00f4cedce82cb29
Jan-David <141488120+JdDXP@users.noreply.github.com>
2023-08-05 11:42:58 +0200

    Add files via upload

---
+++ remove_apps.bat
@@ -0,0 +1,26 @@
+@echo off 
+
+winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c
+winget uninstall Microsoft.549981C3F5F10_8wekyb3d8bbwe                    
+winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe                       
+winget uninstall Microsoft.GetHelp_8wekyb3d8bbwe                           
+winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe                       
+winget uninstall Microsoft.MSPaint_8wekyb3d8bbwe                          
+winget uninstall Microsoft.Microsoft3DViewer_8wekyb3d8bbwe                 
+winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe                
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe      
+winget uninstall Microsoft.People_8wekyb3d8bbwe                           
+winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c                                   
+winget uninstall Microsoft.Wallet_8wekyb3d8bbwe                           
+winget uninstall Microsoft.WindowsAlarms_8wekyb3d8bbwe                     
+winget uninstall Microsoft.WindowsCamera_8wekyb3d8bbwe                     
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe                
+winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe                       
+winget uninstall Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe            
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe                                                  
+winget uninstall Microsoft.OneDrive                                        
+winget uninstall Microsoft.windowscommunicationsapps_8wekyb3d8bbwe       
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe
+
+pause
